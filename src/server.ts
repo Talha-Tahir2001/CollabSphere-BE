0941b8c89f4b204ffc29e56610797f52fb765aa8
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-11-04 00:28:06 +0500

    fix: cors issue

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-import authRoutes from './routes/authRoutes.js';
-import workspaceRoutes from './routes/workspaceRoutes.js';
-import projectRoutes from './routes/projectRoutes.js';
-import profileRoutes from './routes/profileRoutes.js';
-import taskRoutes from './routes/taskRoutes.js';
-import messageRoutes from './routes/messageRoutes.js';
+import authRoutes from "./routes/authRoutes.js";
+import workspaceRoutes from "./routes/workspaceRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
-import chatSocketHandler from './socket/chatSocket.js';
+import chatSocketHandler from "./socket/chatSocket.js";
 
-import connectDB from './config/DB.js';
+import connectDB from "./config/DB.js";
 dotenv.config();
 
 const app = express();
@@ -21,32 +21,39 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL || "http://localhost:5174",
-    methods: ["GET", "POST", "PUT", "DELETE"]
-  }
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
 });
 
 // ✅ Initialize socket.io logic
 chatSocketHandler(io);
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5174",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 connectDB();
 
 console.log("✅ Registering auth routes...");
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/workspaces', workspaceRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/profile', profileRoutes);
-app.use('/api/tasks', taskRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/workspaces", workspaceRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
